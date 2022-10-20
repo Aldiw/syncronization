af2f4fd04c16b8b70f488eab3268a1c7c9dc5e0f
Agapete <arilwijaya20@gmail.com>
2022-10-20 12:25:52 +0700

    init

---
+++ src/components/segara/Segara.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Segara.css';
+function Segara() {
+  return(
+   <>
+   <div className="bg">
+      <img className="imgsegara1" src="../images/assets/image/firstSegara1.png" alt='' />
+      <img className="imgsegara2" src="../images/assets/image/firstSegara2.png" alt='' />
+      <img className="imgsegara3" src="../images/assets/image/firstSegara3.png" alt='' />
+      <img className="imgsegara4" src="../images/assets/image/firstSegara4.png" alt='' />
+      <img className="imgsegara5" src="../images/assets/image/firstSegara5.png" alt='' />
+      <div className="grid">
+        <div className="row">
+          <div className="col">
+            <center>
+              <div className="container">
+              <h1><b>Segara</b></h1>
+              <p className="firstTextSegara-p1 mt-4">se·ga·ra kl n laut(an). Budidaya komoditas laut di ruang tertutup.</p>
+              <p class="firstTextSegara-p2 mt-4"><b>Segara diciptakan untuk mengimbangi habitat laut yang mulai sulit dikelola karena perilaku komoditas yang tidak dapat dikendalikan jika dilepas secara bebas. Misalnya kanibalisme antar kepiting dengan kepiting lain atau ikan dengan ikan lain. Selain itu area budi daya yang semakin terbatas karena ide soal reklamasi yang kurang lebih kasusnya seperti lahan sawah yang mulai beralih fungsi.</b></p>
+            </div>
+            </center>
+          </div>
+        </div>
+      </div>
+   </div>
+
+    <div className="container-p2">
+    <h1><b>Segara</b></h1>
+          <p className="firstTextSegara-p1 mt-4">se·ga·ra kl n laut(an). Budidaya komoditas laut di ruang tertutup.</p>
+          <p class="firstTextSegara-p2 mt-4"><b>Segara diciptakan untuk mengimbangi habitat laut yang mulai sulit dikelola karena perilaku komoditas yang tidak dapat dikendalikan jika dilepas secara bebas. Misalnya kanibalisme antar kepiting dengan kepiting lain atau ikan dengan ikan lain. Selain itu area budi daya yang semakin terbatas karena ide soal reklamasi yang kurang lebih kasusnya seperti lahan sawah yang mulai beralih fungsi.</b></p>
+
+
+    </div>
+
+   </>
+    
+    
+  )
+}
+
+export default Segara
